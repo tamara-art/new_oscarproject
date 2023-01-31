7e1b473714f027a025eff94e49654413a50f157c
tammy-crypo <mass_t@mail.ru>
2023-01-31 23:13:25 +0100

    adds in itemlist new method

--- src/test/java/com/telran/project/fw/ItemListContainerHelper.java
+++ src/test/java/com/telran/project/fw/ItemListContainerHelper.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ItemListContainerHelper extends BaseHelper{
+
+    public static final String ID_TOP_PAGE_DIV_DIV_UL_LI_1_A = "//*[@id=\"top_page\"]/div/div/ul/li[1]/a";
+
     public ItemListContainerHelper(WebDriver webDriver) {
         super(webDriver);
     }
@@ -12,6 +15,7 @@ public class ItemListContainerHelper extends BaseHelper{
         return elementIsPresent(By.id("top_page"));
     }
     public boolean userNameIsPresent() {
-        return elementIsPresent(By.id("top_page"));
+//        return elementIsPresent(By.id("top_page"));
+        return elementIsPresent(By.xpath(ID_TOP_PAGE_DIV_DIV_UL_LI_1_A));
     }
 }
