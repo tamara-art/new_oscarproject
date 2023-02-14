56ab219af11ce280744828c62c3f588a890c2e28
tammy-crypo <mass_t@mail.ru>
2023-02-14 19:04:01 +0100

    Adds Itemcard features

--- src/test/java/com/telran/project/fw/ItemCardHelper.java
+++ src/test/java/com/telran/project/fw/ItemCardHelper.java
@@ -1,4 +1,26 @@
 package com.telran.project.fw;
 
-public class ItemCardHelper {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ItemCardHelper extends BaseHelper{
+    public ItemCardHelper(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void clickOnProductPicture() {
+        click(By.cssSelector("#default > div > div > div > div > section > div > ol > " +
+                "li:nth-child(1) > article > div.image_container.w-100 > a > img"));
+    }
+
+    public void clickOnProductTitle() {
+        click(By.cssSelector("#default > div > div > div > div > section > div > ol > li:nth-child(1)" +
+                " > article > h3 > a"));
+    }
+
+    public void clickOnProductAddToBasketBtn() {
+        click(By.cssSelector("#default > div > div > div > div > section > div > ol > li:nth-child(1) > " +
+                "article > div.product_price.w-100 > form > button"));
+    }
+
 }
--- src/test/java/com/telran/project/steps/ItemCardSteps.java
+++ src/test/java/com/telran/project/steps/ItemCardSteps.java
@@ -1,4 +1,50 @@
 package com.telran.project.steps;
 
+import com.telran.project.fw.ApplicationManager;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class ItemCardSteps {
+    protected static ApplicationManager app;
+
+    public ItemCardSteps(ApplicationManager app) {
+        LoginSteps.app = app;
+    }
+
+    @When("All products page is opened")
+    public void allProductsPageIsOpened() {
+        app.goToAllProductsPage();
+    }
+
+    @Given("Url ist correct")
+    public void urlIstCorrect() {
+        app.goToAllProductsPage();
+    }
+
+    @When("Correct book title in navigation panel")
+    public void correctBookTitleInNavigationPanel() {
+
+    }
+
+    @When("Book title is the same as on all products page")
+    public void bookTitleIsTheSameAsOnAllProductsPage() {
+
+    }
+
+    @When("Prise is the same as on All products page")
+    public void priseIsTheSameAsOnAllProductsPage() {
+
+    }
+
+    @When("The product has in stock label")
+    public void theProductHasInStockLabel() {
+
+    }
+
+    @And("Add to basket is clickable")
+    public void addToBasketIsClickable() {
+    }
 }
